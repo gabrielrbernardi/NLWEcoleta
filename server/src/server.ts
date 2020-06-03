445e4ba85c2a83e6a300ec11d4d9f38e04fe8d5a
gabrielrbernardi <gabrielrbernardi@gmail.com>
2020-06-03 11:45:45 -0300

    Criacao do Backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-app.get('/users', (request, response) => {
-    console.log("listagem de usuarios");
-    response.json([
-        'Joao',
-        'Maria',
-        'Jose',
-        'Joana'
-    ])
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
